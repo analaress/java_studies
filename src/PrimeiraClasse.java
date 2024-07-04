37bd8e02c6eaa065643ff388bd88ac90d9c97522
Ana <analaressads@gmail.com>
2024-07-04 11:42:12 -0300

    first commit

---
+++ src/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.printf("Conclui a aula 01 e agora estou mergulhando em java");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
